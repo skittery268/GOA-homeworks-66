ae3cd70bea36ffe34567d643a1b30a494f715140
skittery268 <sabadzidzikashvili6@gmail.com>
2025-11-21 17:59:08 +0400

    Initial Commit

--- level 115/classwork/app.js
+++
@@ -1,7 +0,0 @@
-const { add, subtract, multiply, divide } = require("./utils/math.js")
-
-console.log(add(10, 15))
-console.log(subtract(20, 15))
-console.log(multiply(10, 2))
-console.log(divide(10, 5))
-
